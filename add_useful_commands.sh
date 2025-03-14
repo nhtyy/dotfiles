2902d311c3af61af07b0c742676fb2c844b822f0
nhtyy <n@push0.dev>
2025-03-14 14:33:29 -0700

    fix: handle bash

--- add_useful_commands.sh
+++ add_useful_commands.sh
@@ -1,17 +1,23 @@
 set -e
 
+
 if [[ $SHELL == *"zsh"* ]]; then
-    echo "zsh"
+    echo "found zsh"
     echo "assuming you have a ~/.zshrc file @ $HOME/.zshrc"
+    RC_FILE=$HOME/.zshrc
+elif [[ $SHELL == *"bash"* ]]; then
+    echo "found bash"
+    echo "assuming you have a ~/.bashrc file @ $HOME/.bashrc"
+    RC_FILE=$HOME/.bashrc
 else
     echo "Unknown shell type $SHELL"
     exit 1
 fi
 
-echo "adding useful commands to $HOME/.zshrc"
+echo "adding useful commands to $RC_FILE"
 
 mkdir -p $HOME/.nhtyy-helpers/bin
 cp -r shell/helpers/* $HOME/.nhtyy-helpers/bin
 
 echo "Amending PATH to include $HOME/.nhtyy-helpers/bin"
-echo "export PATH=$HOME/.nhtyy-helpers/bin:$PATH" >> $HOME/.zshrc
+echo "export PATH=$HOME/.nhtyy-helpers/bin:$PATH" >> $RC_FILE
